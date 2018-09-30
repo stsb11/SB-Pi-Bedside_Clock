062a5819e9798435463ad98714ddf03899a18b30
stsb11 <stephen.brown@bourne-grammar.lincs.sch.uk>
2018-09-30 19:19:14 +0100

    Tidied up repo

--- littst.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-print("Loading...")
-
-with open('quotes.csv', 'rb') as csvfile:
-    content = csv.reader(csvfile, delimiter='|', quotechar='"')
-    quotes = list(content)
-
-print(quotes[0][2])
